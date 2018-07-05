d520aeaa14943f6b88d2fbd078c802286115d1ce
Basant kumar Hota <basahota@users.noreply.github.com>
2018-07-05 22:25:58 +0530

    #code

---
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -0,0 +1,13 @@
+package com.javatechie.app.git_demo;
+
+public class App {
+	public void addData(String data) {
+		if (data.equals(Constant.DATA_TYPE)) {
+			System.out.println("Welcome");
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
