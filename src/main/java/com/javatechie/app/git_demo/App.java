4ae2255d5bed22976f5ee28b30aabd6cc4097bdf
basahota <techbasanta757@gmail.com>
2018-07-05 22:36:44 +0530

    added null check by dev-1

--- src/main/java/com/javatechie/app/git_demo/App.java
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -2,8 +2,10 @@ package com.javatechie.app.git_demo;
 
 public class App {
 	public void addData(String data) {
+		if(data!=null){
 		if (data.equals(Constant.DATA_TYPE)) {
 			System.out.println("Welcome");
+		}
 		}
 	}
 
